8836683e91f5db660115e695c523afca07238112
Amir Soltani <amirsoltanik78@gmail.com>
2023-11-22 13:26:26 +0330

    add stroke width

---
+++ src/TextalignJustifycenter.js
@@ -0,0 +1,156 @@
+import React, { forwardRef } from 'react';
+import PropTypes from 'prop-types';
+import Svg, { Path, G } from 'react-native-svg';
+
+const Bold = ({ color, strokeWidth = 2 }) => (
+  <>
+    <Path
+      d="M21 5.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75ZM21 10.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75ZM21 15.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75ZM21 20.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75Z"
+      fill={color}
+    ></Path>
+  </>
+);
+
+const Broken = ({ color, strokeWidth = 2 }) => (
+  <>
+    <Path
+      d="M3 4.5h18M3 14.5h18M3 19.5h18M13.95 9.5H3M21 9.5h-2.98"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></Path>
+  </>
+);
+
+const Bulk = ({ color, strokeWidth = 2 }) => (
+  <>
+    <Path
+      d="M21 5.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75Z"
+      fill={color}
+    ></Path>
+    <Path
+      opacity=".4"
+      d="M21 10.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75Z"
+      fill={color}
+    ></Path>
+    <Path
+      d="M21 15.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75Z"
+      fill={color}
+    ></Path>
+    <Path
+      opacity=".4"
+      d="M21 20.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75Z"
+      fill={color}
+    ></Path>
+  </>
+);
+
+const Linear = ({ color, strokeWidth = 2 }) => (
+  <>
+    <Path
+      d="M3 4.5h18M3 9.5h18M3 14.5h18M3 19.5h18"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></Path>
+  </>
+);
+
+const Outline = ({ color, strokeWidth = 2 }) => (
+  <>
+    <Path
+      d="M21 5.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75ZM21 10.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75ZM21 15.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75ZM21 20.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75Z"
+      fill={color}
+    ></Path>
+  </>
+);
+
+const TwoTone = ({ color, strokeWidth = 2 }) => (
+  <>
+    <Path
+      opacity=".4"
+      d="M3 4.5h18"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></Path>
+    <Path
+      d="M3 9.5h18"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></Path>
+    <Path
+      opacity=".4"
+      d="M3 14.5h18"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></Path>
+    <Path
+      d="M3 19.5h18"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></Path>
+  </>
+);
+
+const chooseVariant = (variant, color) => {
+  switch (variant) {
+    case 'Bold':
+      return <Bold color={color} />;
+
+    case 'Broken':
+      return <Broken color={color} />;
+
+    case 'Bulk':
+      return <Bulk color={color} />;
+
+    case 'Linear':
+      return <Linear color={color} />;
+
+    case 'Outline':
+      return <Outline color={color} />;
+
+    case 'TwoTone':
+      return <TwoTone color={color} />;
+
+    default:
+      return <Linear color={color} />;
+  }
+};
+
+const TextalignJustifycenter = forwardRef(({ variant, color, size, ...rest }, ref) => {
+  return (
+    <Svg
+      {...rest}
+      xmlns="http://www.w3.org/2000/svg"
+      ref={ref}
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      {chooseVariant(variant, color)}
+    </Svg>
+  );
+});
+TextalignJustifycenter.propTypes = {
+  variant: PropTypes.oneOf(['Linear', 'Bold', 'Broken', 'Bulk', 'Outline', 'TwoTone']),
+  color: PropTypes.string,
+  size: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+TextalignJustifycenter.defaultProps = {
+  variant: 'Linear',
+  color: 'currentColor',
+  size: '24',
+};
+TextalignJustifycenter.displayName = 'TextalignJustifycenter';
+export default TextalignJustifycenter;
