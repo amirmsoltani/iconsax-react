83553608e294198ab9814b61a77adcfc82d42d25
Amir Soltani <amirsoltanik78@gmail.com>
2023-11-22 13:53:12 +0330

    fix issue

---
+++ dist/esm/TextalignJustifyleft.js
@@ -0,0 +1,171 @@
+import { _ as _objectWithoutProperties, a as _extends } from './_rollupPluginBabelHelpers-3bc641ae.js';
+import React, { forwardRef } from 'react';
+import PropTypes from 'prop-types';
+import Svg, { Path } from 'react-native-svg';
+
+var _excluded = ["variant", "color", "size"];
+
+var Bold = function Bold(_ref) {
+  var color = _ref.color;
+      _ref.strokeWidth;
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Path, {
+    d: "M12 5.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h9c.41 0 .75.34.75.75s-.34.75-.75.75ZM12 10.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h9c.41 0 .75.34.75.75s-.34.75-.75.75ZM21 15.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75ZM21 20.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75Z",
+    fill: color
+  }));
+};
+
+var Broken = function Broken(_ref2) {
+  var color = _ref2.color,
+      _ref2$strokeWidth = _ref2.strokeWidth,
+      strokeWidth = _ref2$strokeWidth === void 0 ? 2.5 : _ref2$strokeWidth;
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Path, {
+    d: "M12 4.5H3M12 9.5H3M21 14.5H3M10.05 19.5H21M3 19.5h2.98",
+    stroke: color,
+    strokeWidth: strokeWidth,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  }));
+};
+
+var Bulk = function Bulk(_ref3) {
+  var color = _ref3.color;
+      _ref3.strokeWidth;
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Path, {
+    d: "M12 5.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h9c.41 0 .75.34.75.75s-.34.75-.75.75Z",
+    fill: color
+  }), /*#__PURE__*/React.createElement(Path, {
+    opacity: ".4",
+    d: "M12 10.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h9c.41 0 .75.34.75.75s-.34.75-.75.75Z",
+    fill: color
+  }), /*#__PURE__*/React.createElement(Path, {
+    d: "M21 15.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75Z",
+    fill: color
+  }), /*#__PURE__*/React.createElement(Path, {
+    opacity: ".4",
+    d: "M21 20.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75Z",
+    fill: color
+  }));
+};
+
+var Linear = function Linear(_ref4) {
+  var color = _ref4.color,
+      _ref4$strokeWidth = _ref4.strokeWidth,
+      strokeWidth = _ref4$strokeWidth === void 0 ? 2.5 : _ref4$strokeWidth;
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Path, {
+    d: "M12 4.5H3M12 9.5H3M21 14.5H3M21 19.5H3",
+    stroke: color,
+    strokeWidth: strokeWidth,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  }));
+};
+
+var Outline = function Outline(_ref5) {
+  var color = _ref5.color;
+      _ref5.strokeWidth;
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Path, {
+    d: "M12 5.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h9c.41 0 .75.34.75.75s-.34.75-.75.75ZM12 10.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h9c.41 0 .75.34.75.75s-.34.75-.75.75ZM21 15.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75ZM21 20.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75Z",
+    fill: color
+  }));
+};
+
+var TwoTone = function TwoTone(_ref6) {
+  var color = _ref6.color,
+      _ref6$strokeWidth = _ref6.strokeWidth,
+      strokeWidth = _ref6$strokeWidth === void 0 ? 2.5 : _ref6$strokeWidth;
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Path, {
+    opacity: ".4",
+    d: "M12 4.5H3",
+    stroke: color,
+    strokeWidth: strokeWidth,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  }), /*#__PURE__*/React.createElement(Path, {
+    d: "M12 9.5H3",
+    stroke: color,
+    strokeWidth: strokeWidth,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  }), /*#__PURE__*/React.createElement(Path, {
+    opacity: ".4",
+    d: "M21 14.5H3",
+    stroke: color,
+    strokeWidth: strokeWidth,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  }), /*#__PURE__*/React.createElement(Path, {
+    d: "M21 19.5H3",
+    stroke: color,
+    strokeWidth: strokeWidth,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  }));
+};
+
+var chooseVariant = function chooseVariant(variant, color) {
+  switch (variant) {
+    case 'Bold':
+      return /*#__PURE__*/React.createElement(Bold, {
+        color: color
+      });
+
+    case 'Broken':
+      return /*#__PURE__*/React.createElement(Broken, {
+        color: color
+      });
+
+    case 'Bulk':
+      return /*#__PURE__*/React.createElement(Bulk, {
+        color: color
+      });
+
+    case 'Linear':
+      return /*#__PURE__*/React.createElement(Linear, {
+        color: color
+      });
+
+    case 'Outline':
+      return /*#__PURE__*/React.createElement(Outline, {
+        color: color
+      });
+
+    case 'TwoTone':
+      return /*#__PURE__*/React.createElement(TwoTone, {
+        color: color
+      });
+
+    default:
+      return /*#__PURE__*/React.createElement(Linear, {
+        color: color
+      });
+  }
+};
+
+var TextalignJustifyleft = /*#__PURE__*/forwardRef(function (_ref7, ref) {
+  var variant = _ref7.variant,
+      color = _ref7.color,
+      size = _ref7.size,
+      rest = _objectWithoutProperties(_ref7, _excluded);
+
+  return /*#__PURE__*/React.createElement(Svg, _extends({}, rest, {
+    xmlns: "http://www.w3.org/2000/svg",
+    ref: ref,
+    width: size,
+    height: size,
+    viewBox: "0 0 24 24",
+    fill: "none"
+  }), chooseVariant(variant, color));
+});
+TextalignJustifyleft.propTypes = {
+  variant: PropTypes.oneOf(['Linear', 'Bold', 'Broken', 'Bulk', 'Outline', 'TwoTone']),
+  color: PropTypes.string,
+  size: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+};
+TextalignJustifyleft.defaultProps = {
+  variant: 'Linear',
+  color: 'currentColor',
+  size: '24'
+};
+TextalignJustifyleft.displayName = 'TextalignJustifyleft';
+
+export { TextalignJustifyleft as default };
---
+++ dist/esm/TextalignJustifyright.js
@@ -0,0 +1,171 @@
+import { _ as _objectWithoutProperties, a as _extends } from './_rollupPluginBabelHelpers-3bc641ae.js';
+import React, { forwardRef } from 'react';
+import PropTypes from 'prop-types';
+import Svg, { Path } from 'react-native-svg';
+
+var _excluded = ["variant", "color", "size"];
+
+var Bold = function Bold(_ref) {
+  var color = _ref.color;
+      _ref.strokeWidth;
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Path, {
+    d: "M21 5.25h-9c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h9c.41 0 .75.34.75.75s-.34.75-.75.75ZM21 10.25h-9c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h9c.41 0 .75.34.75.75s-.34.75-.75.75ZM21 15.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75ZM21 20.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75Z",
+    fill: color
+  }));
+};
+
+var Broken = function Broken(_ref2) {
+  var color = _ref2.color,
+      _ref2$strokeWidth = _ref2.strokeWidth,
+      strokeWidth = _ref2$strokeWidth === void 0 ? 2.5 : _ref2$strokeWidth;
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Path, {
+    d: "M12 4.5h9M12 9.5h9M3 14.5h18M13.95 19.5H3M21 19.5h-2.98",
+    stroke: color,
+    strokeWidth: strokeWidth,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  }));
+};
+
+var Bulk = function Bulk(_ref3) {
+  var color = _ref3.color;
+      _ref3.strokeWidth;
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Path, {
+    d: "M21 5.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75Z",
+    fill: color
+  }), /*#__PURE__*/React.createElement(Path, {
+    opacity: ".4",
+    d: "M21.001 10.25h-9.47c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h9.47c.41 0 .75.34.75.75s-.34.75-.75.75Z",
+    fill: color
+  }), /*#__PURE__*/React.createElement(Path, {
+    d: "M21 15.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75Z",
+    fill: color
+  }), /*#__PURE__*/React.createElement(Path, {
+    opacity: ".4",
+    d: "M21.001 20.25h-9.47c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h9.47c.41 0 .75.34.75.75s-.34.75-.75.75Z",
+    fill: color
+  }));
+};
+
+var Linear = function Linear(_ref4) {
+  var color = _ref4.color,
+      _ref4$strokeWidth = _ref4.strokeWidth,
+      strokeWidth = _ref4$strokeWidth === void 0 ? 2.5 : _ref4$strokeWidth;
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Path, {
+    d: "M12 4.5h9M12 9.5h9M3 14.5h18M3 19.5h18",
+    stroke: color,
+    strokeWidth: strokeWidth,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  }));
+};
+
+var Outline = function Outline(_ref5) {
+  var color = _ref5.color;
+      _ref5.strokeWidth;
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Path, {
+    d: "M21 5.25h-9c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h9c.41 0 .75.34.75.75s-.34.75-.75.75ZM21 10.25h-9c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h9c.41 0 .75.34.75.75s-.34.75-.75.75ZM21 15.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75ZM21 20.25H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75Z",
+    fill: color
+  }));
+};
+
+var TwoTone = function TwoTone(_ref6) {
+  var color = _ref6.color,
+      _ref6$strokeWidth = _ref6.strokeWidth,
+      strokeWidth = _ref6$strokeWidth === void 0 ? 2.5 : _ref6$strokeWidth;
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Path, {
+    opacity: ".4",
+    d: "M12 4.5h9",
+    stroke: color,
+    strokeWidth: strokeWidth,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  }), /*#__PURE__*/React.createElement(Path, {
+    d: "M12 9.5h9",
+    stroke: color,
+    strokeWidth: strokeWidth,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  }), /*#__PURE__*/React.createElement(Path, {
+    opacity: ".4",
+    d: "M3 14.5h18",
+    stroke: color,
+    strokeWidth: strokeWidth,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  }), /*#__PURE__*/React.createElement(Path, {
+    d: "M3 19.5h18",
+    stroke: color,
+    strokeWidth: strokeWidth,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  }));
+};
+
+var chooseVariant = function chooseVariant(variant, color) {
+  switch (variant) {
+    case 'Bold':
+      return /*#__PURE__*/React.createElement(Bold, {
+        color: color
+      });
+
+    case 'Broken':
+      return /*#__PURE__*/React.createElement(Broken, {
+        color: color
+      });
+
+    case 'Bulk':
+      return /*#__PURE__*/React.createElement(Bulk, {
+        color: color
+      });
+
+    case 'Linear':
+      return /*#__PURE__*/React.createElement(Linear, {
+        color: color
+      });
+
+    case 'Outline':
+      return /*#__PURE__*/React.createElement(Outline, {
+        color: color
+      });
+
+    case 'TwoTone':
+      return /*#__PURE__*/React.createElement(TwoTone, {
+        color: color
+      });
+
+    default:
+      return /*#__PURE__*/React.createElement(Linear, {
+        color: color
+      });
+  }
+};
+
+var TextalignJustifyright = /*#__PURE__*/forwardRef(function (_ref7, ref) {
+  var variant = _ref7.variant,
+      color = _ref7.color,
+      size = _ref7.size,
+      rest = _objectWithoutProperties(_ref7, _excluded);
+
+  return /*#__PURE__*/React.createElement(Svg, _extends({}, rest, {
+    xmlns: "http://www.w3.org/2000/svg",
+    ref: ref,
+    width: size,
+    height: size,
+    viewBox: "0 0 24 24",
+    fill: "none"
+  }), chooseVariant(variant, color));
+});
+TextalignJustifyright.propTypes = {
+  variant: PropTypes.oneOf(['Linear', 'Bold', 'Broken', 'Bulk', 'Outline', 'TwoTone']),
+  color: PropTypes.string,
+  size: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+};
+TextalignJustifyright.defaultProps = {
+  variant: 'Linear',
+  color: 'currentColor',
+  size: '24'
+};
+TextalignJustifyright.displayName = 'TextalignJustifyright';
+
+export { TextalignJustifyright as default };
